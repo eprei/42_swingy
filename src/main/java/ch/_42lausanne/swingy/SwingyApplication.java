0ab10bbe3d3762337f0db27824df124e9d7dd2ec
eprei <93380002+eprei@users.noreply.github.com>
2024-03-28 16:23:07 +0100

    set up Spring Boot

---
+++ src/main/java/ch/_42lausanne/swingy/SwingyApplication.java
@@ -0,0 +1,13 @@
+package ch._42lausanne.swingy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwingyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwingyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ch/_42lausanne/swingy/SwingyApplicationTests.java
@@ -0,0 +1,13 @@
+package ch._42lausanne.swingy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwingyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
